5eff08ac9c76059acf80b0b1b59dc53e5b05b03f
Colin Nico Schmid <mail@mala23.me>
2018-11-20 13:36:39 +0100

    read csv to json

--- geocode.js
+++ geocode.js
@@ -1,13 +1,22 @@
 var Request = require("request")
 var fs = require("fs")
+var csvtojson = require("csvtojson")
+
+csvtojson().fromFile('./csv_source/solar.csv').then((jsonObj) => {
+  console.log(jsonObj)
+})
 
 Request.get("https://geocode.xyz/Rueras+Graubünden?json=1?region=CH", (error, response, body) => {
+  
   if (error) {
     return console.dir(error)
   }
+
   var apiResponse = JSON.parse(body)
+
   console.log(apiResponse.longt)
   console.log(apiResponse.latt)
+
   fs.appendFile("coordinates.csv", apiResponse.longt + ", " + apiResponse.latt + "," + "\n", function(error) {
     if (error) {
       console.log("append failed")
