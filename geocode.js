1740255652e997e87cdccbe6774a169787351183
Colin Nico Schmid <mail@mala23.me>
2018-11-20 10:02:25 +0100

    first attempt on node – fail

---
+++ geocode.js
@@ -0,0 +1,8 @@
+var Request = require("request")
+
+Request.get("https://geocode.xyz/Rueras+Graubünden?json=1?region=CH", error, response, body) => {
+  if (error) {
+    return console.dir(error)
+  }
+  console.dir(JSON.parse(body))
+})
