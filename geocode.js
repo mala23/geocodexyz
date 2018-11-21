81aceccffd1cc5e2146731f44423e311c000323f
Colin Nico Schmid <mail@mala23.me>
2018-11-21 11:01:28 +0100

    fix all the things

--- geocode.js
+++ geocode.js
@@ -4,28 +4,32 @@ var readcsv = require('readcsv')
 
 readcsv(true, './csv_source/solar.csv', (err, data) => {
   if(err) { return console.log(err) }
-
   data.forEach(line => {
-    console.log(line.Anlage_Ort);
+
+    setTimeout(function() {
+    //console.log(line.Anlage_Ort);
+    Request.get(('https://geocode.xyz/' + line.Anlage_PLZ + '?json=1?region=CH'), (error, response, body) => {
+      
+      if (error) {
+        return console.dir(error)
+      }
+
+      var apiResponse = JSON.parse(body)
+
+      console.log(apiResponse.latt)
+      console.log(apiResponse.longt)
+
+      fs.appendFile('coordinates.csv', line.Anlage_Ort + ", " + line.Anlage_PLZ + ", " +  apiResponse.latt + ", " + apiResponse.longt + "," + "\n", function(error) {
+
+        if (error) {
+          console.log('append failed')
+        } else {
+          console.log('done')
+        }
+
+      })
+    })
+    }, 1000)
   })
 })
 
-Request.get("https://geocode.xyz/Rueras+Graubünden?json=1?region=CH", (error, response, body) => {
-  
-  if (error) {
-    return console.dir(error)
-  }
-
-  var apiResponse = JSON.parse(body)
-
-  console.log(apiResponse.latt)
-  console.log(apiResponse.longt)
-
-  fs.appendFile('coordinates.csv', apiResponse.latt + ", " + apiResponse.longt + "," + "\n", function(error) {
-    if (error) {
-      console.log("append failed")
-    } else {
-      console.log("done")
-    }
-  })
-})
