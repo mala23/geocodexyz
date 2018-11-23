72a001aedabc3fe28f1a450270f8399f651d6432
Colin Nico Schmid <mail@mala23.me>
2018-11-23 16:54:24 +0100

    fails because not all places have zips

--- geocode.js
+++ geocode.js
@@ -7,21 +7,20 @@ const geoCode = (lines, next) => {
     if(!line) {
       return next()
     }
-    console.log(encodeURIComponent(line.Ort))
-    console.log(encodeURIComponent(line.PLZ))
-    request.get(('https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(line.PLZ) + ',+Switzerland' + '&key=AIzaSyCDvW_uvyPe-L_bjFrcYiWLsbOP1sbq7B4'), (error, response, body) => {
+    console.log(encodeURIComponent(line.Location))
+    request.get(('https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(line.Location) + ',+Switzerland' + '&key=AIzaSyCDvW_uvyPe-L_bjFrcYiWLsbOP1sbq7B4'), (error, response, body) => {
 
       if (error) {
         return console.dir(error)
       }
 
       let apiResponse = JSON.parse(body)
-      let lat = apiResponse.results[0].geometry.location.lat
-      let lng = apiResponse.results[0].geometry.location.lng
+      console.log(apiResponse)
+      let zip = apiResponse.results[0].address_components[5]
 
-      console.log(lat)
-      console.log(lng)
-      fs.appendFile('coordinates.csv', line.Ort + "," + line.PLZ + ", " +  lat + ", " + lng + "," + "\n", function(error) {
+      console.log(zip)
+      /*
+      fs.appendFile('data_zip.csv', line.Ort + "," + line.PLZ + ", " +  lat + ", " + lng + "," + "\n", function(error) {
 
         if (error) {
           console.log('append failed')
@@ -30,6 +29,7 @@ const geoCode = (lines, next) => {
         }
 
       })
+      */
 
       lines.shift()
 
@@ -40,7 +40,7 @@ const geoCode = (lines, next) => {
     })
 }
 
-readcsv(true, './csv_source/solar_pt2.csv', (err, data) => {
+readcsv(true, './csv_source/zips/water_data.csv', (err, data) => {
   if(err) { return console.log(err) }
   geoCode(data, () => {
     console.log('we have data')
